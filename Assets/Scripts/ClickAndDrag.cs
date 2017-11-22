6d03e926096bce4581a7637f00d31463f6275081
E. Ung <evee.ung@gmail.com>
2017-11-21 20:13:04 -0500

    Script added for click and drag.

--- Assets/Scripts/ClickAndDrag.cs
+++ Assets/Scripts/ClickAndDrag.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class ClickAndDrag : MonoBehaviour {
 
+	float yPosition;
+
 	// Use this for initialization
 	void Start () {
+
+		yPosition = transform.position.y;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
 	}
 
 	void OnMouseDrag()
 	{
+
 		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
+
+		//This lets you click on the object and drag it around:
+		Vector3 tempVector = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
+
+		transform.position = new Vector3 (tempVector.x, yPosition, tempVector.z);
 
 	}
 
