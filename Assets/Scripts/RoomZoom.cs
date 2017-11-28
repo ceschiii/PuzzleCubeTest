e0a9805835bd273b1efa9ecc4640b991f6911fc2
Francesca <francesca.carletto.leon@gmail.com>
2017-11-27 22:29:09 -0500

    Transition into first room is working! Click and drag functionality on placeholder character. Added a light in the room for visibility. Adjusted colliders.
    ZoomRoom script now disables front wall and house collider.

--- Assets/Scripts/RoomZoom.cs
+++ Assets/Scripts/RoomZoom.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class RoomZoom : MonoBehaviour {
     public Animator anim;
+    public GameObject wall;
+    BoxCollider collider;
 
 	// Use this for initialization
 	void Start () {
-      
+        collider = GetComponent<BoxCollider>();
 	}
 	
 	// Update is called once per frame
@@ -18,5 +20,13 @@ public class RoomZoom : MonoBehaviour {
     void OnMouseDown()
     {
         anim.Play("CameraZoom");
+        StartCoroutine(ByeByeWall());
+    }
+
+    IEnumerator ByeByeWall()
+    {
+        yield return new WaitForSeconds(1.5f);
+        wall.SetActive(false);
+        collider.enabled = false;
     }
 }
