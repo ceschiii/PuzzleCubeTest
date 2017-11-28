a9fe5b55750aa08bee44ca923514b1d20c923442
Francesca <francesca.carletto.leon@gmail.com>
2017-11-27 21:12:32 -0500

    Zoom into room on click script and functionality working in test scene!

--- Assets/Scripts/RoomZoom.cs
+++ Assets/Scripts/RoomZoom.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomZoom : MonoBehaviour {
+    public Animator anim;
 
 	// Use this for initialization
 	void Start () {
-		
+      
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnMouseDown()
+    {
+        anim.Play("CameraZoom");
+    }
 }
