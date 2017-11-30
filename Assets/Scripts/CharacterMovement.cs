2e9f5ed2ac9598b9a360f64e1be8e1addf674b39
Francesca <francesca.carletto.leon@gmail.com>
2017-11-30 10:48:46 -0500

    Added CharacterMovement script for example of NavMesh reference. Fixed camera animation on opening click

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System;
+
+public class CharacterMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator animator;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector3 moving = agent.velocity;     
+        animator.SetFloat("Moving", moving.magnitude);
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                print(hit.transform.name);
+                if (hit.transform.name == "path")
+                {
+                    agent.destination = hit.point;
+                }
+
+            }
+        }
+    }
+}
