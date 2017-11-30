39c2a990f22bf4957abac9fb2e8e6825c4798ad2
E. Ung <evee.ung@gmail.com>
2017-11-30 11:32:20 -0500

    Fixed character mesh and movement.
    Next to do: fix character collision box!

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,32 +6,51 @@ using System;
 
 public class CharacterMovement : MonoBehaviour
 {
+	float yPosition;
+	public bool IsMoving = false;
+
     NavMeshAgent agent;
     Animator animator;
 
     void Start()
     {
+		yPosition = transform.position.y;
         agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Vector3 moving = agent.velocity;     
-        animator.SetFloat("Moving", moving.magnitude);
-        if (Input.GetMouseButtonDown(0))
+        //animator.SetFloat("Moving", moving.magnitude);
+        if (IsMoving)
         {
             RaycastHit hit;
 
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
-            {
-                print(hit.transform.name);
-                if (hit.transform.name == "path")
-                {
-                    agent.destination = hit.point;
-                }
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) {
+				agent.destination = new Vector3 (hit.point.x, yPosition, hit.point.z);
+
+				//transform.position = new Vector3 (hit.point.x, yPosition, hit.point.z);
+			}
+
+
+
+//            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+//            {
+//                print(hit.transform.name);
+//                if (hit.transform.name == "path")
+//                {
+//                    agent.destination = hit.point;
+//                }
 
             }
         }
-    }
+ 
+	void OnMouseDown(){
+		IsMoving = !IsMoving;
+	}
+	
 }
+
+	
