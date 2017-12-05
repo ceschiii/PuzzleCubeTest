9d9635150df1ebf3f133898096e37a250f2ce3df
E. Ung <evee.ung@gmail.com>
2017-12-04 19:44:43 -0500

    Return to original click and drag script!

--- Assets/Scripts/ClickAndDrag.cs
+++ Assets/Scripts/ClickAndDrag.cs
@@ -17,22 +17,22 @@ public class ClickAndDrag : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (IsMoving) {
-//			float distance_to_screen = Camera.main.WorldToScreenPoint (gameObject.transform.position).z;
+//		if (IsMoving) {
+////			float distance_to_screen = Camera.main.WorldToScreenPoint (gameObject.transform.position).z;
+////
+////			//This lets you click on the object and drag it around:
+////			Vector3 tempVector = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+////
+////			transform.position = new Vector3 (tempVector.x, yPosition, tempVector.z);
 //
-//			//This lets you click on the object and drag it around:
-//			Vector3 tempVector = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+//			RaycastHit hit;
+//			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//			if (Physics.Raycast (ray, out hit)) {
+//				transform.position = new Vector3 (hit.point.x, yPosition, hit.point.z);;
 //
-//			transform.position = new Vector3 (tempVector.x, yPosition, tempVector.z);
-
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			if (Physics.Raycast (ray, out hit)) {
-				transform.position = new Vector3 (hit.point.x, yPosition, hit.point.z);;
-
-			}
-
-		}
+//			}
+//
+//		}
 
 	}
 
@@ -43,13 +43,13 @@ public class ClickAndDrag : MonoBehaviour {
 
 	void OnMouseDrag()
 	{
-//		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-//
-//		//This lets you click on the object and drag it around:
-//		Vector3 tempVector = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
-//
-//		//makes sure you can't move on the y axis
-//		transform.position = new Vector3 (tempVector.x, yPosition, tempVector.z);
+		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+		//This lets you click on the object and drag it around:
+		Vector3 tempVector = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
+
+		//makes sure you can't move on the y axis
+		transform.position = new Vector3 (tempVector.x, yPosition, tempVector.z);
 
 
 	}
